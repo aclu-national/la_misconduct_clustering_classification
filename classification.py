274f8b9f9533def4a3b345a68d4ec169837f9240
Elijah Appelson <124807267+appelson@users.noreply.github.com>
2024-07-24 17:46:33 -0400

    Adding new embeddings and train-test stratification

--- classification.py
+++ classification.py
@@ -1,6 +1,6 @@
-''' 
+'''
 Code to classify misconduct allegations using a Support Vector Classifier and multi-label classification.
-Using random search cross validation with 10 folds. 
+Using random search cross-validation with 10 folds.
 '''
 
 # -----------------------------------------------------------------------
@@ -11,10 +11,12 @@ Using random search cross validation with 10 folds.
 # Importing Packages
 import pandas as pd
 import re
+from skmultilearn.model_selection import iterative_train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix, roc_auc_score, ConfusionMatrixDisplay, multilabel_confusion_matrix
@@ -25,24 +27,24 @@ from nltk.tokenize import word_tokenize
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from transformers import AutoTokenizer,AutoModel
+from sentence_transformers import SentenceTransformer
+
 
 # ------------------------ Defining Text Cleaning -----------------------------
 
-# Defining stop words, lemmatization, and preprocess from 
-# https://medium.com/@20pd11/1f8776a84ddb
+# Defining stop words, lemmatization, and preprocess functions
 def remove_stopwords(text):
     words = word_tokenize(text)
     stop_words = set(stopwords.words('english'))
     filtered_words = [word for word in words if word.lower() not in stop_words]
-    filtered_text = ' '.join(filtered_words)
-    return filtered_text
+    return ' '.join(filtered_words)
 
 def lemmatize_text(text):
     lemmatizer = WordNetLemmatizer()
     words = word_tokenize(text)
     lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
-    lemmatized_text = ' '.join(lemmatized_words)
-    return lemmatized_text
+    return ' '.join(lemmatized_words)
 
 def preprocess(text):
     text = str(text.split(':')[-1])
@@ -51,14 +53,12 @@ def preprocess(text):
     text = re.sub(r'[^\w\s]', '', text)
     text = re.sub(r'[\d_]+', '', text)
     text = remove_stopwords(text)
-    text = lemmatize_text(text)
-    return text
+    return lemmatize_text(text)
 
 # --------------------- Loading and Cleaning Data -----------------------------
 
 # Reading the data
 df = pd.read_csv("labelled_data.csv")
-df.iloc[[14]]
 
 # Cleaning the allegation and allegation descriptions
 df['allegation_clean'] = df['allegation'].dropna().apply(preprocess)
@@ -67,7 +67,7 @@ df['description_clean'] = df['allegation_desc'].dropna().apply(preprocess)
 # Combine the cleaned allegation and description into one
 df['combined'] = df['allegation_clean'].fillna('') + " " + df['description_clean'].fillna('')
 
-# Turning the classifications into a list of classification for multi-label
+# Turning the classifications into a list of classifications for multi-label
 df['combined_list'] = df['classification'].apply(lambda x: [item.strip() for item in x.split(',')])
 
 # Creating the MultiLabelBinarizer
@@ -79,29 +79,34 @@ binary_matrix = mlb.fit_transform(df['combined_list'])
 # Creating a dataframe out of the MultiLabelBinarizer
 binary_df = pd.DataFrame(binary_matrix, columns=mlb.classes_)
 
-# Concatinating our dataframe with binary_df
-df = pd.concat([df[['combined']], binary_df], axis=1)
-
 # Removing the Miscellaneous Allegation variable
-df = df.drop(labels="Miscellaneous Allegation", axis=1)
+if "Miscellaneous Allegation" in binary_df.columns:
+    binary_df = binary_df.drop(columns=["Miscellaneous Allegation"])
+
+# Concatenating our dataframe with binary_df
+df = pd.concat([df[['combined']], binary_df], axis=1)
 
 # ------------------- Splitting into train-test -----------------------------
 
-# Split the data into training and test sets with a 80-20 split
+# Split the data into training and test sets with an 80-20 split
 test_split = 0.2
-train_df, test_df = train_test_split(df, test_size=test_split, random_state=1)
+X = df[['combined']].values
+y = df.drop(['combined'], axis=1).values
+X_train, y_train, X_test, y_test = iterative_train_test_split(X, y, test_size=test_split)
 
-# Defining the columns
-text_column = 'combined'
-label_columns = [col for col in df.columns if col != text_column]
-
-# Defining the text and labels
-train_texts = train_df[text_column]
-train_labels = train_df[label_columns].astype(float)
-test_texts = test_df[text_column]
-test_labels = test_df[label_columns].astype(float)
+# Converting back to DataFrame for consistency
+train_df = X_train.flatten()
+train_labels = y_train
+test_df =  X_test.flatten()
+test_labels = y_test
 
 # ----------------- Creating Support Vector Classifier ------------------------
+
+# Three different tokenizations / embeddings
+tfidf = TfidfVectorizer(max_features=2000)
+minilm = SentenceTransformer('all-MiniLM-L6-v2')
+bert = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
 
 # Defining the Support Vector Classifier
 svc = SVC(probability=True)
@@ -109,8 +114,8 @@ svc = SVC(probability=True)
 # Defining the possible parameters
 param_dist = {
     'classifier__estimator__C': uniform(0.1, 10),  
-    'classifier__estimator__kernel': ['linear', 'rbf'], 
-    'classifier__estimator__gamma': ['scale', 'auto'] 
+    'classifier__estimator__kernel': ['linear', 'rbf','poly'], 
+    'classifier__estimator__gamma': ['scale', 'auto']
 }
 
 # Defining the MultiOutputClassifier
@@ -118,7 +123,7 @@ multi_target_svc = MultiOutputClassifier(svc)
 
 # Creating our pipeline
 pipeline = Pipeline([
-    ('vectorizer', TfidfVectorizer(max_features=500)),
+    ('vectorizer', minilm),
     ('classifier', multi_target_svc)
 ])
 
@@ -134,17 +139,24 @@ random_search = RandomizedSearchCV(
 )
 
 # Fitting the random search cross validation
-random_search.fit(train_texts, train_labels)
+random_search.fit(train_df, train_labels)
 
 # Pulling the best model
 pipeline = random_search.best_estimator_
-pipeline
+
+# ----------------- Creating Random Forest Classifier ------------------------
+
+# ----------------- Creating XG Boost Classifier ------------------------
+
+# ----------------- Creating RNN Classifier ------------------------
+
+# ----------------- Creating BERT Classifier ------------------------
 
 # ------------------------ Broad Metrics --------------------------
 
 # Predicting
-train_preds = pipeline.predict(train_texts)
-test_preds = pipeline.predict(test_texts)
+train_preds = pipeline.predict(train_df)
+test_preds = pipeline.predict(test_df)
 
 # Counting how many predictions are all 0's 
 np.sum(np.all(test_preds == 0, axis=1))
@@ -174,37 +186,34 @@ overall_metrics = pd.DataFrame(data, index=metrics)
 
 # ---------------------- Variable-Specific Metrics --------------------------
 
+label_columns = binary_df.columns
+
 # Defining the metrics_data list
 metrics_data = []
 
 for i, label in enumerate(label_columns):
-  
-    # Definign test matrics
-    test_precision = precision_score(test_labels[label], test_preds[:, i], average='binary')
-    test_recall = recall_score(test_labels[label], test_preds[:, i], average='binary')
-    test_accuracy = accuracy_score(test_labels[label], test_preds[:, i])
-    test_confusion = confusion_matrix(test_labels[label], test_preds[:, i])
+    # Defining test metrics
+    test_precision = precision_score(test_labels[:, i], test_preds[:, i], average='binary', zero_division=0)
+    test_recall = recall_score(test_labels[:, i], test_preds[:, i], average='binary', zero_division=0)
+    test_accuracy = accuracy_score(test_labels[:, i], test_preds[:, i])
+    test_confusion = confusion_matrix(test_labels[:, i], test_preds[:, i])
     
     def extract_metrics(conf_matrix):
-        if conf_matrix.shape == (2, 2): 
+        tn = fp = fn = tp = 0
+        if conf_matrix.shape == (2, 2):
             tn, fp, fn, tp = conf_matrix.ravel()
+        elif conf_matrix.shape == (1, 2):  # Only negative cases in ground truth
+            tn, fp = conf_matrix.ravel()
+        elif conf_matrix.shape == (2, 1):  # Only positive cases in ground truth
+            fn, tp = conf_matrix.ravel()
+        elif conf_matrix.shape == (1, 1):  # Only one type in both prediction and ground truth
+            if len(set(test_labels[:, i])) == 1 and len(set(test_preds[:, i])) == 1:
+                if test_labels[0, i] == 0:
+                    tn = conf_matrix[0, 0]
+                else:
+                    tp = conf_matrix[0, 0]
         else:
-          # Dealing with when there are not both 0 and 1 
-            tn = fp = fn = tp = 0
-            if conf_matrix.shape[0] == 1:
-                if len(conf_matrix[0]) == 2:
-                    fp = conf_matrix[0][1]
-                    tn = conf_matrix[0][0]
-                else:
-                    tp = conf_matrix[0][0]
-            elif conf_matrix.shape[1] == 1:
-                if len(conf_matrix) == 2:
-                    tn = conf_matrix[0][0]
-                    fp = conf_matrix[1][0]
-                else:
-                    tp = conf_matrix[1][0]
-            else:
-                raise ValueError("Unknown Error")
+            raise ValueError("Unknown Error")
         return tn, fp, fn, tp
     
     test_tn, test_fp, test_fn, test_tp = extract_metrics(test_confusion)
@@ -231,10 +240,10 @@ for i, label in enumerate(label_columns):
         'Test FN Rate': test_fn_rate
     })
 
-# ---------------------- Using the model --------------------------
-
 # Converting the list into a dataframe of variable metrics
 variable_metrics = pd.DataFrame(metrics_data)
+
+# ---------------------- Using the model --------------------------
 
 # Creating a list of classes without "Miscellaneous Allegation"
 filtered_classes = [cls for cls in mlb.classes_ if cls != "Miscellaneous Allegation"]
@@ -252,4 +261,6 @@ def predict(text, pipeline):
       return("Miscellaneous Allegation")
 
 # Testing Model on text
-predict("Weapon Misuse", pipeline)
+predict("Weapon e", pipeline)
+
+
